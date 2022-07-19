76bd1196cbdbf1a08a49c875d68bf19325561924
RowanZeng <zpxoci0619@gmail.com>
2022-07-19 16:47:08 +0800

    0719 0447

--- src/App.js
+++ src/App.js
@@ -10,16 +10,31 @@
 // import RefsForm from "./ex-0715-5/RefsForm";
 // import CC from "./ex-0718-1/CC";
 // import FC from "./ex-0718-1/FC"
+// import BSTest from "./ex-0719-3/BSTest";
+// import User from "./ex-0718-2/User";
 
-import User from "./ex-0718-2/User";
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Product from './pages/Product'
 
 function App() {
     return (
-        <>
-            {/* <CC /> */}
-            {/* <FC /> */}
-            <User/>
-        </>
+        // <>
+        //     {/* <CC /> */}
+        //     {/* <FC /> */}
+        //     {/* <User/> */}
+        //     <BSTest />
+        // </>
+        <BrowserRouter>
+            <Route>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="product" element={<Product />} />
+            </Route>
+        </BrowserRouter>
     );
 }
 
